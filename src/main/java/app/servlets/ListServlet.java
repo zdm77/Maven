2b86e8f95dfbe241bc04dc7c3764fad06b0961f0
user2 <zdm77@mail.ru>
2018-07-01 00:20:14 +0700

    ignore

--- src/main/java/app/servlets/ListServlet.java
+++ src/main/java/app/servlets/ListServlet.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class ListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        System.out.println("тест");
 
         Model model = Model.getInstance();
         //Model model = new Model(true);
